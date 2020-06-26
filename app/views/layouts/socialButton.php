4bb004e036e0d16bcd888aee3bfb5833a664fb77
Alexey <info@webskill24.ru>
2020-06-26 14:40:38 +0300

    Почему мы продолжаем делать это!!!! АЛЁ

--- app/views/layouts/socialButton.php
+++ app/views/layouts/socialButton.php
@@ -4,11 +4,14 @@
         <label class="menu-open-button" for="menu-open">
             <i class="far fa-comment-alt"></i>
         </label>
-        <a class="menu-item phone_share_btn" href="viber://add?number=79832693953">
+        <a class="menu-item phone_share_btn" href="tel:+79832693953">
+            <i class="fas fa-phone-alt phone__icon" aria-hidden="true"></i>
+        </a>
+        <a class="menu-item phone_share_btn" href="https://viber.click/79832693953">
             <i class="fab fa-viber" aria-hidden="true"></i>
         </a>
         <a class="menu-item whatsapp_share_btn" href="https://wa.me/79832693953">
-            <i class="fa fa-whatsapp icon" aria-hidden="true"></i>
+            <i class="fa fa-whatsapp " aria-hidden="true"></i>
         </a>
     </nav>
 </div>
